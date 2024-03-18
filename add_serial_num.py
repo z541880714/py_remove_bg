841f82e5088956120a75362f8726122733eefc31
z541880714 <541880714@qq.com>
2024-03-18 21:30:43 +0800

    变更抠图算法

---
+++ add_serial_num.py
@@ -0,0 +1,47 @@
+# encoding='utf-8'
+from PIL import Image, ImageDraw, ImageFont
+from os import path
+import os
+
+resolution = 1024  # 统一输出分辨率
+logo_size = 200
+logo_margin = 10
+out_dir = 'out_serial_num/'
+
+if not path.exists(out_dir):
+    os.mkdir(out_dir)
+if not path.exists('res/'):
+    os.mkdir('res/')
+
+
+def handle_img(pic_name):
+    no_bg = Image.open(f'res/{pic_name}').convert('RGB')
+    x, y = no_bg.size
+    max_size = max(x, y)
+    rate = y / x
+    x_ = x if rate < 1 else int(round(y / rate))
+    y_ = y if rate > 1 else int(round(x * rate))
+    x_margin = int(0.5 * (max_size - x_))
+    y_margin = int(0.5 * (max_size - y_))
+    new_img = Image.new('RGB', (max_size, max_size), (255, 255, 255))
+    box = (x_margin, y_margin,
+           min(x_margin + x, max_size - x_margin),
+           min(x_margin + y, max_size - y_margin))
+    new_img.paste(no_bg, box, mask=None)
+    new_img = new_img.resize((resolution, resolution), Image.BILINEAR)
+    draw = ImageDraw.Draw(new_img)
+    font_style = ImageFont.truetype('arial.ttf', 30, encoding='utf-8')
+    draw.text((20, resolution - 50), pic_name[:pic_name.index('.')], '#000000', font=font_style)
+    new_img.save(f'{out_dir}/{pic_name}')
+
+
+if __name__ == '__main__':
+    index = 1
+    size = len(os.listdir('res'))
+    for f in os.listdir('res'):
+        # if not f == 'ATHB430290-F776-IvyZhang.jpg':
+        #     continue
+        print(f'正在处理:{f} 图片总数:{size},当前进度:{index}')
+        index += 1
+        # remove_background(f)
+        handle_img(f)
--- clean_bg.py
+++ clean_bg.py
@@ -1,11 +1,23 @@
 # encoding='utf-8'
+import numpy
 from rembg.bg import remove
 from PIL import Image, ImageDraw, ImageFont
 from os import path
 import os
+from transparent_background import Remover
 
-resolution = 2000  # 统一输出分辨率
+remover = Remover()
 
+
+def process_bg(img):
+    img = remover.process(img, type='[0, 0, 0]', threshold=0.6)
+    img = remover.process(img, type='rgba')
+    return img
+
+
+resolution = 1024  # 统一输出分辨率
+logo_size = 200
+logo_margin = 10
 if not path.exists('out/'):
     os.mkdir('out/')
 if not path.exists('res/'):
@@ -20,39 +32,49 @@ def remove_bg(path_in):
 def logoImage():
     logo = Image.open('assets/logo2.png')
     x, y = logo.size
-    logo = logo.resize((400, int(y / x * 400)))
+    logo = logo.resize((logo_size, int(y / x * logo_size)))
     return logo
 
 
 def handle_img(pic_name):
-    no_bg = remove_bg(f'res/{pic_name}')
+    img_path = f'res/{pic_name}'
+    img = Image.open(img_path).convert("RGB")
+    no_bg = process_bg(img)
     x, y = no_bg.size
     max_size = max(x, y)
     rate = y / x
-    x_ = x if rate < 1 else int(y / rate)
-    y_ = y if rate > 1 else int(x * rate)
-
-    x_margin = int(0.5 * (max_size - x_))
-    y_marging = int(0.5 * (max_size - y_))
-
+    x_ = x if rate < 1 else int(numpy.round(y / rate))
+    y_ = y if rate > 1 else int(numpy.round(x * rate))
+    x_margin = int(numpy.floor(0.5 * (max_size - x_)))
+    y_margin = int(numpy.floor(0.5 * (max_size - y_)))
     logo = logoImage()
-
     new_img = Image.new('RGB', (max_size, max_size), (255, 255, 255))
-    new_img.paste(no_bg, (0 + x_margin, 0 + y_marging, x_margin + x_, y_marging + y_), mask=no_bg)
+    box = (x_margin, y_margin,
+           min(x_margin + x, max_size - x_margin),
+           min(x_margin + y, max_size - y_margin))
+    new_img.paste(no_bg, box, mask=no_bg)
     new_img = new_img.resize((resolution, resolution), Image.BILINEAR)
-    new_img.paste(logo, (resolution - logo.width, 20, resolution, 20 + logo.height), mask=logo)
-
+    new_img.paste(logo, (resolution - logo.width, logo_margin, resolution, logo_margin + logo.height),
+                  mask=logo)
     draw = ImageDraw.Draw(new_img)
-    font_style = ImageFont.truetype('arial.ttf', 50, encoding='utf-8')
-    draw.text((20, resolution - 100), pic_name[:pic_name.index('.')], '#000000', font=font_style)
+    font_style = ImageFont.truetype('arial.ttf', 30, encoding='utf-8')
+    draw.text((20, resolution - 50), pic_name[:pic_name.index('.')], '#000000', font=font_style)
     new_img.save(f'./out/{pic_name}')
 
 
-if __name__ == '__main__':
+def remove_background(img_name):
+    img_in_path = f'res/{img_name}'
+    img_out_path = f'out/{img_name}'
+    os.system(f"python -m backgroundremover.cmd.cli -i {img_in_path} -o {img_out_path}")
 
+
+if __name__ == '__main__':
     index = 1
     size = len(os.listdir('res'))
     for f in os.listdir('res'):
+        # if not f == 'ATHB430290-F776-IvyZhang.jpg':
+        #     continue
         print(f'正在处理:{f} 图片总数:{size},当前进度:{index}')
         index += 1
+        # remove_background(f)
         handle_img(f)
