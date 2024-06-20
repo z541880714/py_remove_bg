ed6f68afbf7d2f2068e526d58a734c708b5c357b
z541880714 <541880714@qq.com>
2024-06-21 00:35:48 +0800

    logo 显示可选.

--- build.bat
+++ build.bat
@@ -13,10 +13,10 @@ if not exist dist\%pyname%\assets md dist\%pyname%\assets
 xcopy assets dist\%pyname%\assets /e/i/y
 
 %pyinstaller% %qrname%.py -y
-xcopy dist\%qrname% dist\%pyname%  /s/e/i/y
 
-rd /s /q dist\%qrname%\
 
+::xcopy dist\%qrname% dist\%pyname%  /s/e/i/y
+::rd /s /q dist\%qrname%\
 
 ::7z a out\py_remove.7z dist\%pyname%
 
--- qrcodeGenerator.py
+++ qrcodeGenerator.py
@@ -18,8 +18,9 @@ def resize(img, size_t):
 
 parser = argparse.ArgumentParser()
 parser.add_argument("str", type=str, help="二维码的字符串内容")
-parser.add_argument("logoPath", type=str, help="logo文件路径")
 parser.add_argument("output", type=str, help="二维码文件输出路径")
+parser.add_argument("--logoPath", type=str, default='', help="logo文件路径")
+
 args = parser.parse_args()
 
 str = args.str
@@ -30,26 +31,27 @@ print("qr str:", str, ' output:', output)
 img = qrcode.make(data=str)
 img.save(output)
 
-# 将logo 放入 二维码中间.
-crImg = cv2.imdecode(np.fromfile(output, dtype=np.uint8), cv2.IMREAD_COLOR)
-crImg = cv2.resize(crImg, (500, 500))
-print('crImg shape:', crImg.shape)
+if len(args.logoPath) > 0 and os.path.exists(args.logoPath):
+    # 将logo 放入 二维码中间.
+    crImg = cv2.imdecode(np.fromfile(output, dtype=np.uint8), cv2.IMREAD_COLOR)
+    crImg = cv2.resize(crImg, (500, 500))
+    print('crImg shape:', crImg.shape)
 
-logo = cv2.imdecode(np.fromfile(args.logoPath, dtype=np.uint8), cv2.IMREAD_COLOR)
+    logo = cv2.imdecode(np.fromfile(args.logoPath, dtype=np.uint8), cv2.IMREAD_COLOR)
 
-logo_gray = cv2.cvtColor(logo, cv2.COLOR_BGR2GRAY)
-ret, logo_mask = cv2.threshold(logo_gray, 20, 255, cv2.THRESH_BINARY)
+    logo_gray = cv2.cvtColor(logo, cv2.COLOR_BGR2GRAY)
+    ret, logo_mask = cv2.threshold(logo_gray, 20, 255, cv2.THRESH_BINARY)
 
-mask_inv = cv2.bitwise_not(logo_mask)
-logo_bg = cv2.cvtColor(mask_inv, cv2.COLOR_GRAY2BGR)
-logo_fg = cv2.bitwise_and(logo, logo, mask=logo_mask)
-logo = cv2.add(logo_bg, logo_fg)
+    mask_inv = cv2.bitwise_not(logo_mask)
+    logo_bg = cv2.cvtColor(mask_inv, cv2.COLOR_GRAY2BGR)
+    logo_fg = cv2.bitwise_and(logo, logo, mask=logo_mask)
+    logo = cv2.add(logo_bg, logo_fg)
 
-target_size = 120
-logo = resize(logo, target_size)
+    target_size = 120
+    logo = resize(logo, target_size)
 
-h, w, _ = logo.shape
-left = (500 - w) // 2
-top = (500 - h) // 2
-crImg[top: top + h, left:left + w] = logo
-cv2.imencode('.jpg', crImg)[1].tofile(output)
+    h, w, _ = logo.shape
+    left = (500 - w) // 2
+    top = (500 - h) // 2
+    crImg[top: top + h, left:left + w] = logo
+    cv2.imencode('.jpg', crImg)[1].tofile(output)
